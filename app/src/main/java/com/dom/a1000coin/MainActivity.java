bda37e32a6234067f38b4a22d961df525b214089
DoommeR <success-07@list.ru>
2018-11-13 23:39:10 +0300

    initial commit

---
+++ app/src/main/java/com/dom/a1000coin/MainActivity.java
@@ -0,0 +1,82 @@
+package com.dom.a1000coin;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import java.util.Collection;
+//git test
+public class MainActivity extends AppCompatActivity {
+
+
+    private ItemFragment fragments[] = new ItemFragment[4];
+    private int selected;
+
+    private ItemFragment getFragment(int pos) {
+        if (fragments[pos] == null) {
+            fragments[pos] = ItemFragment.newInstance(pos + 1);
+        }
+        selected = pos;
+        return fragments[pos];
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, getFragment(0)).addToBackStack(null).commit();
+                    return true;
+                case R.id.navigation_give:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, getFragment(1)).addToBackStack(null).commit();
+                    return true;
+                case R.id.navigation_receieve:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, getFragment(2)).addToBackStack(null).commit();
+                    return true;
+                case R.id.navigation_restore:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, getFragment(3)).addToBackStack(null).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState != null) {
+            selected = savedInstanceState.getInt("SELECTED");
+            for (int i = 0; i < fragments.length; i++) {
+                fragments[i] = (ItemFragment) getSupportFragmentManager().getFragment(savedInstanceState, "F" + i);
+                if(fragments[i]!=null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, fragments[i]).addToBackStack(null).commit();
+                }
+            }
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, getFragment(selected)).addToBackStack(null).commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("SELECTED", selected);
+        for (int i = 0; i < fragments.length; i++) {
+            if(fragments[i]!=null){
+                getSupportFragmentManager().putFragment(outState, "F" + i, fragments[i]);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dom/a1000coin/coin/CoinContent.java
@@ -0,0 +1,66 @@
+package com.dom.a1000coin.coin;
+
+import android.media.Image;
+import android.support.annotation.AnyRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.RawRes;
+import android.widget.ImageView;
+
+
+import com.dom.a1000coin.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CoinContent {
+
+    public static final List<Coin> ITEMS = new ArrayList<Coin>();
+    public static final Map<String, Coin> ITEM_MAP = new HashMap<String, Coin>();
+    private static final int COUNT = 25;
+
+    static {
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createCoin(i));
+        }
+    }
+
+    private static Coin createCoin(int position) {
+        return new Coin(String.valueOf(position), "coin № " + position, makeDetails(position), R.drawable.test);
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Coin: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    private static void addItem(Coin item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    public static class Coin {
+        public final String id;
+        public final int img;
+        public final String content;
+        public final String details;
+
+        public Coin(String id, String content, String details, @DrawableRes int imgRes) {
+            this.id = id;
+            this.content = content;
+            this.details = details;
+            this.img=  imgRes;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
